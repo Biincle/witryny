8e1f5c6c9ae8e6185c6f0f05f3bf934856caea60
Biincle <pawelek599@gmail.com>
2017-05-19 12:16:48 +0200

    Add files via upload

---
+++ statki/game.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < 10; i++){
+  for (var j = 0; j < 10; i++) {
+    $('#plansza1').append('<div class="grid">test</div>');
+  }
+  $('#plansza1').append('<div style="clear:both;"</div>');
+}
